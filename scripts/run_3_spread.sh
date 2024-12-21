23cadcfc4790c61e4d2561911d96545f7358f7f0
dynamicDr <1102310490@qq.com>
2024-12-21 17:06:01 +0000

    1

--- scripts/run_3_spread.sh
+++ scripts/run_3_spread.sh
@@ -1,3 +1,2 @@
 python 1.MAPPO_MPE/MAPPO_MPE_main.py \
---num_of_agents 3 \
---lr 1e-3
+--num_of_agents 3
--- scripts/run_5_spread.sh
+++ scripts/run_5_spread.sh
@@ -1,3 +1,3 @@
 python 1.MAPPO_MPE/MAPPO_MPE_main.py \
 --num_of_agents 5 \
---lr 1e-3
+--max_train_steps 50000000
