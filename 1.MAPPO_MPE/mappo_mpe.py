b4087f102f81049ec368f73922249f96363738e1
dynamicDr <1102310490@qq.com>
2024-12-21 17:16:18 +0000

    1

--- 1.MAPPO_MPE/mappo_mpe.py
+++ 1.MAPPO_MPE/mappo_mpe.py
@@ -292,7 +292,7 @@ class MAPPO_MPE:
         return actor_inputs, critic_inputs
 
     def save_model(self, env_name, number, seed, total_steps, abs_path):
-        torch.save(self.actor.state_dict(), "{}/model/MAPPO_actor_env_{}_number_{}_seed_{}_step_{}k.pth".format(abs_path,env_name, number, seed, int(total_steps / 1000)))
+        torch.save(self.actor.state_dict(), "{}/model/{}_{}k.npy".format(abs_path,number,int(total_steps / 1000)))
 
-    def load_model(self, env_name, number, seed, step):
-        self.actor.load_state_dict(torch.load("{}/model/MAPPO_actor_env_{}_number_{}_seed_{}_step_{}k.pth".format(abs_path,env_name, number, seed, step)))
+    def load_model(self, env_name, number, seed, step, abs_path):
+        self.actor.load_state_dict(torch.load("{}/model/{}_{}k.npy".format(abs_path,number,int(step / 1000))))
--- scripts/run_5_spread.sh
+++
@@ -1,3 +0,0 @@
-python 1.MAPPO_MPE/MAPPO_MPE_main.py \
---num_of_agents 5 \
---max_train_steps 50000000
